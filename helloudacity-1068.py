88edfa885e27367675bccc535d5a6ccabca50c66
Felipe Goncalves Marques <marques.goncalves.felipe@gmail.com>
2015-09-29 12:34:51 -0300

    Templates - Looping in Jinja2

--- helloudacity-1068.py
+++ helloudacity-1068.py
@@ -3,17 +3,9 @@ import os
 import jinja2
 import webapp2
 
-template_dir = os.path.join(os.path.dirname(_file_), 'templates')
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
 
-form_html = """
-<form>
-<h2>Add a Food</h2>
-<input type="text" name="food">
-%s
-<button>Add</button>
-</form>
-"""
 hidden_html = """
 <input type="hidden" name="food" value="%s">
 """
@@ -32,23 +24,34 @@ class Handler(webapp2.RequestHandler):
 	def write(self, *a, **kw):
 		self.response.out.write(*a, **kw)
 
+	def render_str(self, template, **params):
+		t = jinja_env.get_template(template)
+		return t.render(params)
+	def render(self, template, **kw):
+		self.write(self.render_str(template, **kw))
+
 class MainPage(Handler):
 	def get(self):
-		output = form_html
-		output_hidden = ""
+		n = self.request.get("n")
+		if n and n.isdigit():
+			n = int(n)
+		self.render("shopping_list.html", n=n)
 
-		items = self.request.get_all("food")
-		if items:
-			output_items = ""
-			for item in items:
-				output_hidden += hidden_html%item
-				output_items += item_html%item
-			output_shopping = shopping_list_html % output_items
-			output += output_shopping
+		# output = form_html
+		# output_hidden = ""
 
-		output = output % output_hidden
+		# items = self.request.get_all("food")
+		# if items:
+		# 	output_items = ""
+		# 	for item in items:
+		# 		output_hidden += hidden_html%item
+		# 		output_items += item_html%item
+		# 	output_shopping = shopping_list_html % output_items
+		# 	output += output_shopping
+
+		# output = output % output_hidden
 		
-		self.write(output)
+		# self.write(output)
 
 app = webapp2.WSGIApplication([('/', MainPage)
 							   ],
