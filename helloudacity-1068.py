319dc463ce12aa26cf346686ccb0714c9e2c9cd7
Felipe Goncalves Marques <marques.goncalves.felipe@gmail.com>
2015-12-30 20:38:56 +1000

    Project ready to start final task

--- helloudacity-1068.py
+++ helloudacity-1068.py
@@ -48,80 +48,10 @@ def verify_cookie(cookie):
 	hash_val = cookie.split("|")[1]
 	return hashlib.sha256(val + SECRET).hexdigest() == hash_val
 
-def gmaps_img(points):
-    markers = '&'.join('markers=%s,%s' % (p.lat,p.lon) for p in points)
-    return GMAPS_URL + markers
-
-def get_coord(ip):
-	url = IP_URL + ip
-	content = None
-	try:
-		content = urllib2.urlopen(url).read()
-	except urllib2.URLError:
-		return
-
-	if content:
-		d = minidom.parseString(content)
-   		if d.getElementsByTagName("status")[0].childNodes[0].nodeValue == "success":
-   			return db.GeoPt(d.getElementsByTagName("lat")[0].childNodes[0].nodeValue,
-   							d.getElementsByTagName("lon")[0].childNodes[0].nodeValue)
-
-def top_arts(art = None):
-	key = 'top'
-	arts = memcache.get(key)
-	if (arts is None) or art:
-		if arts is None:
-			logging.error("DB QUERY")
-			arts = list(db.GqlQuery("SELECT * FROM Art ORDER BY created DESC"))
-		if art:
-			arts.insert(0, art)
-		memcache.set(key, arts)
-	return arts
-last_time_cache = time.time()
-last_time_cache_post = {}
-def get_post(post_id):
-	global last_time_cache_post
-	post = memcache.get(post_id)
-	if post is None:
-		post = Entry.get_by_id(int(post_id))
-		if post:
-			last_time_cache_post[post_id] = time.time()
-			logging.error("Caching ok %.0f" % last_time_cache_post[post_id])
-			memcache.set(post_id, post)
-		else:
-			return
-	return post
-def top_post(post = None):
-	global last_time_cache
-	key = 'post'
-	posts = memcache.get(key)
-	if (posts is None) or post:
-		if posts is None:
-			last_time_cache = time.time()
-			logging.error("DB QUERY")
-			posts = list(db.GqlQuery("SELECT * FROM Entry ORDER BY created DESC"))
-		if post:
-			posts.insert(0, post)
-		memcache.set(key, posts)
-	return posts
-class Blog(db.Model):
-	name = db.StringProperty()
 class User(db.Model):
 	username = db.StringProperty(required = True)
 	password_hash = db.StringProperty(required = True)
 	email = db.EmailProperty()
-
-class Art(db.Model):
-	title = db.StringProperty(required = True)
-	art = db.StringProperty(required = True, multiline = True)
-	created = db.DateTimeProperty(auto_now_add = True)
-	coords = db.GeoPtProperty()
-
-class Entry(db.Model):
-	title = db.StringProperty(required = True)
-	content = db.StringProperty(required = True, multiline = True)
-	created = db.DateTimeProperty(auto_now_add = True)
-	last_modified = db.DateTimeProperty(auto_now = True)
 
 class Handler(webapp2.RequestHandler):
 	def write(self, *a, **kw):
@@ -131,22 +61,7 @@ class Handler(webapp2.RequestHandler):
 		return t.render(params)
 	def render(self,template,**kw):
 		self.write(self.render_str(template,**kw))
-class HandlerPost(Handler):
-	def render_posts(self, posts):
-		posts_rendered = ""
-		for post in posts:
-			posts_rendered = posts_rendered + self.render_str("post.html", post = post, 
-				time = post.last_modified.strftime("%a %b %d %H:%M:%S %Y"))
-		return posts_rendered
-class HandlerWithCookie(Handler):
-	def set_cookie_valid(self, user):
-		val_cookie = make_valid_cookie(str(user.key().id()))
-		self.set_cookie(val_cookie)
-	def set_cookie_empty(self):
-		self.set_cookie("")
-	def set_cookie(self,val):
-		self.response.headers.add_header('Set-Cookie', '%s=%s; Path=/' % ("user_id", val))
-		
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
@@ -160,6 +75,7 @@ class Welcome(Handler):
 			self.render("welcome.html", username = str(user.username))
 		else:
 			self.redirect("/blog/signup")
+
 class SignUp(HandlerWithCookie):
 	def write_form(self, username="", error_username="", error_password="", error_verify="", email="", error_email=""):
 		self.render("signup.html", username = username, 
@@ -232,113 +148,10 @@ class Logout(HandlerWithCookie):
 		self.set_cookie_empty()
 		self.redirect("/blog/signup")				
 
-class Ascii(Handler):
-	def write_form(self, title="", error_title="", content="", error_art=""):
-		arts = top_arts()
-		points = filter(None, (a.coords for a in arts))
-		img_url = ""
-		if points:
-			img_url = gmaps_img(points)		
-		self.render("ascii.html", arts = arts, title=title, error_title=error_title,
-								 content = content, error_art=error_art, img_url = img_url)
-	def get(self):
-		self.write_form()
-	def post(self):
-		title = self.request.get("title")
-		art = self.request.get("art")
-
-		error_title = "Insert a title."
-		error_art = "Insert an Art"
-
-		if title:
-			error_title = ""
-		if art:
-			error_art = ""
-
-		if error_title or error_art:
-			self.write_form(title = title, error_title = error_title, content = art, error_art = error_art)
-		else:
-			art = Art(title = title, art = art)
-			coords = get_coord(self.request.remote_addr)
-			if coords:
-				art.coords = coords
-			art.put()
-			logging.error("Added to the DB")
-			top_arts(art = art)
-			self.redirect("/ascii")
-
-class NewPost(Handler):
-	
-	def get(self):
-		self.render("newpost.html")
-	
-	def post(self):
-		title = self.request.get("subject")
-		content = self.request.get("content")
-		
-		if title and content:
-			entry = Entry(title = title, content = content)
-			entry.put()
-			top_post(post = entry)
-			self.redirect("/blog/%s" % (entry.key().id()))
-		else:
-			self.render("newpost.html", title = title, content = content, error_message = "Insert both title and content!")
-class Post(HandlerPost):
-	def get(self, post_id):
-		global last_time_cache_post
-		post = get_post(post_id)
-		if post:
-			posts_rendered = self.render_posts([post])
-			self.render("blog.html", posts = posts_rendered, time = time.time() - last_time_cache_post[post_id])
-		else:
-			self.error(404)
-			return
-class Blog(HandlerPost):
-	def get(self):
-		posts = top_post()
-		posts_rendered = self.render_posts(posts)
-		self.render("blog.html", posts = posts_rendered, time = time.time() - last_time_cache)
-class BlogJson(Handler):
-	def render_post(self, post):
-		if post:
-			return self.render_str("postJ.html", post = post, time_created = post.created.strftime("%a %b %d %H:%M:%S %Y"),
-								time_last = post.last_modified.strftime("%a %b %d %H:%M:%S %Y"))
-	def get(self):
-		self.response.headers['Content-Type'] = 'application/json: charset=UTF-8'
-		posts = list(Entry.all().order('-created'))
-		json = "["
-		for post in posts[0:len(posts) - 2]:
-			json = json + self.render_post(post) + ", "
-		json = json + self.render_post(posts[-1]) + "]"
-		self.write(json)
-
-class PostJson(Handler):
-	def get(self, post_id):
-		self.response.headers['Content-Type'] = 'application/json: charset=UTF-8'
-		post = Entry.get_by_id(int(post_id))
-		if post:
-			self.render("postJ.html", post = post, time_created = post.created.strftime("%a %b %d %H:%M:%S %Y"),
-								time_last = post.last_modified.strftime("%a %b %d %H:%M:%S %Y"))
-		else:
-			self.error(404)
-			return
-
-class Flush(webapp2.RequestHandler):
-	def get(self):
-		memcache.flush_all()
-		self.redirect("/blog")
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/blog/signup', SignUp),
-    ('/blog/welcome', Welcome),
-    ('/blog/login', Login),
-    ('/blog/logout', Logout),
-    ('/blog/newpost', NewPost),
-    ('/blog/([0-9]+)', Post),
-    ('/blog/([0-9]+).json', PostJson),
-    ('/blog/', Blog),
-    ('/blog', Blog),
-    ('/blog/.json', BlogJson),
-    ('/ascii', Ascii),
-    ('/blog/flush', Flush)], 
+    ('/signup', SignUp),
+    ('/welcome', Welcome),
+    ('/login', Login),
+    ('/logout', Logout)], 
     debug=True)
